99d18c0c94107a17332964302a48441202c87c3b
Amr Mustafa <amrmostafa1311@gmail.com>
2023-08-30 14:31:02 +0300

    implemented routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import SingleDrug from "./pages/SingleDrug";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+import ResetPassword from "./pages/ResetPassword";
+import PharmacyProfile from "./pages/PharmacyProfile";
+import Map from "./pages/Map";
+import UserProfile from "./pages/UserProfile";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/drug/:id" element={<SingleDrug />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/saved" element={<Saved />} />
+        <Route path="/resetpassword" element={<ResetPassword />} />
+        <Route path="/pharmacyprofile" element={<PharmacyProfile />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/userprofile" element={<UserProfile />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  return (
+    <nav className="flex gap-4">
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/signup">Signup</NavLink>
+      <NavLink to="/login">Login</NavLink>
+      <NavLink to="/cart">Cart</NavLink>
+      <NavLink to="/saved">Saved</NavLink>
+      <NavLink to="/map">Map</NavLink>
+      <NavLink to="/userprofile">UserProfile</NavLink>
+    </nav>
+  );
 };
 
 export default Navbar;
